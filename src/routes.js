3b5e50587215cbd403eea1e77910e6feecf4185a
supermancbs <clark.sacktor@gmail.com>
2016-08-15 11:56:18 -0400

    add forms and dumb components

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router'
 import App from './App'
 import CreateAccount from './components/presentational/CreateAccountForm'
 import LogInForm from './components/presentational/LogInForm'
-
+import LogOut from './components/container/LogOut'
+import Profile from './components/presentational/Profile'
 
 export default (
   <Route path="/" component={App} >
+    <Route path="logout" component={LogOut}/>
     <Route path="login" component={LogInForm}/>
     <Route path="newuser" component={CreateAccount}/>
   </Route>
