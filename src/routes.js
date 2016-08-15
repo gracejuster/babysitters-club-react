761dd357c3e0279121aadce3f3b4599c1a626bbc
grahk93 <grace.juster@flatironschool.com>
2016-08-15 15:22:57 -0400

    changed profile to dashboard

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import App from './App'
 import CreateAccount from './components/presentational/CreateAccountForm'
 import LogInForm from './components/presentational/LogInForm'
 import LogOut from './components/container/LogOut'
-import Profile from './components/presentational/Profile'
+import Dashboard from './components/presentational/Dashboard'
 
 export default (
   <Route path="/" component={App} >
@@ -12,7 +12,7 @@ export default (
     {/* put this back soon! */}
     <Route path="login" component={LogInForm}/>
     <Route path="newuser" component={CreateAccount}/>
-    <Route path="user/:username" component={Profile}/>
+    <Route path="user/:username" component={Dashboard}/>
 
     {/* becomes a prop in profile - this.props.params --> :username */}
   </Route>
