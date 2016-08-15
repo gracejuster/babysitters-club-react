057491aa7dc133fff1f8ea36e9a8ade267fa0e53
gyeon <gyeon19@gmail.com>
2016-08-15 14:48:41 -0400

    redux forms

--- src/index.js
+++ src/index.js
@@ -2,23 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-import { combineReducers } from 'redux'
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form';
 import users from './reducers/users'
 import parents from './reducers/parents'
 import babysitters from './reducers/babysitters'
 import { Router, browserHistory } from 'react-router'
 import Routes from './routes'
 
-
 const reducer = combineReducers({
   users: users,
   parents: parents,
-  babysitters: babysitters
+  babysitters: babysitters,
+  form: formReducer
 })
 
 const store = createStore(reducer)
-
 
 ReactDOM.render(
   <Provider store={store}>
