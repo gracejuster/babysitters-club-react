4ea5bb7c49d21ff019fabfd974d757982eb7ef71
gyeon <gyeon19@gmail.com>
2016-08-17 14:11:49 -0400

    more styling, not working all that well

--- src/components/container/CreateAccountForm.js
+++ src/components/container/CreateAccountForm.js
@@ -17,12 +17,26 @@ const CreateAccountForm = class extends Component {
           <input type='text' placeholder='E-MAIL' className="form-control" {...email}/>
           <input type='text' placeholder='USERNAME' className="form-control" {...username}/>
           <input type='text' placeholder='PASSWORD' className="form-control" {...password}/>
-
-            <label className="radioLabel">PARENT</label>
+          <div class="radioContainer">
+            <ul>
+            <li>
+              <input type="radio" {...user_type} id="one" value='PARENT' checked={user_type.value === 'parent'}/>
+              <label for="one">PARENT</label>
+              <div className="check"></div>
+            </li>
+            <li>
+              <input type='radio' {...user_type} id="two" value='BABYSITTER' checked={user_type.value === 'babysitter'}/>
+              <label for="two">BABYSITTER</label>
+              <div className="check"><div className="inside"></div></div>
+            </li>
+          </ul>
+          </div>
+            {/* <label className="radioLabel">PARENT</label>
             <input type='radio' {...user_type} value='PARENT' checked={user_type.value === 'parent'}/>
+            <div className="check"></div>
             <label className="radioLabel">BABYSITTER</label>
-            <input type='radio' {...user_type} value='BABYSITTER' checked={user_type.value === 'babysitter'}/>
-          
+            <input type='radio' {...user_type} value='BABYSITTER' checked={user_type.value === 'babysitter'}/> */}
+
           <input id='submit' type='submit' className="btn btn-primary" value="CREATE ACCOUNT"/>
         </form>
       </div>
