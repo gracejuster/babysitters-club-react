0c9061d058d10293030db375a243eca3f9ddf641
chriswigington <chriswigington@gmail.com>
2016-08-18 13:46:23 -0400

    Added framework for Network component

--- src/components/presentational/Network.js
+++ src/components/presentational/Network.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux'
 
-const Network = class extends Component {
-	render(){
-		return (
-  		<div className='Network'>
-    			"This is my network"
-  		</div>
-  	)
+let Network = ({name, }) => {
+
+	return (
+		<div className='Network'>
+  			"This is my network"
+		</div>
+	)
+}
+
+Network.propTypes = {
+	name: PropTypes.string.isRequired
+}
+
+// Everything below here should probably be in a NetworkContainer component
+
+const mapStateToProps = (state) => {
+	return {
+		// prop: state.something
 	}
 }
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		// clickOnName: (userId) => { dispatch(goToProfile(userId))}
+	}
+}
+
+Network = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Network)
 
 export default Network
