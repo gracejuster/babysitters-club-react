55d4157dfaaf6cfeebec260f33a63800ae4e20ba
grahk93 <grace.juster@flatironschool.com>
2016-08-20 13:59:37 -0400

    pending and confirmed bookings display

--- src/components/presentational/ParentDashboard.js
+++ src/components/presentational/ParentDashboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from '../container/SearchBar'
 import Bookings from './Bookings'
-// import auth from '../container/AuthenticationResource'
 import {connect} from 'react-redux'
 
 let ParentDashboard = class extends Component{
