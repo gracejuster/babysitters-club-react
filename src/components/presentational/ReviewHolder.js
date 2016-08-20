650a97c0075451386967a9299d500421b8dfeb01
chriswigington <chriswigington@gmail.com>
2016-08-20 11:52:49 -0400

    Laid groundwork for ReviewHolder'

--- src/components/presentational/ReviewHolder.js
+++ src/components/presentational/ReviewHolder.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Review from './Review.js'
 
 const ReviewHolder = ({reviews}) => {
+  let reviewList = reviews.map((review) => {
+		return <Review  />
+	})
   return(
     <div className="ReviewHolder">
-    	"These are my reviews"
-      <Review />
+    	<h1>Reviews</h1>
+      {reviewList}
     </div>
   )
 }
