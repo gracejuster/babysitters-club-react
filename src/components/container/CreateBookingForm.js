1952697bc2065af5bf13b42e021f766bba1e1f9b
gyeon <gyeon19@gmail.com>
2016-08-20 18:55:07 -0400

    fixed confirm bookings, made tables

--- src/components/container/CreateBookingForm.js
+++ src/components/container/CreateBookingForm.js
@@ -11,7 +11,7 @@ const CreateBookingForm = class extends Component {
   }
 
   render(){
-    const { fields: {duration, desired_time }, handleSubmit } = this.props;
+    const { fields: {duration, desired_time, desired_date}, handleSubmit } = this.props;
     return(
       <div className='CreateBookingForm'>
       <form id='form' onSubmit={handleSubmit(this.handleCreateBookingSubmit.bind(this))}>
@@ -19,6 +19,8 @@ const CreateBookingForm = class extends Component {
         <input type='text' placeholder='DURATION' className="form-control" {...duration}/>
         {/* <label>Desired Time: </label> */}
         <input type='text' placeholder='DESIRED TIME' className="form-control" {...desired_time}/>
+        <input type='text' placeholder='DESIRED DATE' className="form-control" {...desired_date}/>
+
         <input id='submit' type='submit' className="btn btn-primary" value='REQUEST BOOKING' />
       </form>
       </div>
@@ -37,6 +39,7 @@ export default reduxForm({
   form: 'CreateBookingForm',
   fields: [
     'duration',
-    'desired_time'
+    'desired_time',
+    'desired_date'
   ],
 }, mapStateToProps, { createBooking })(CreateBookingForm);
