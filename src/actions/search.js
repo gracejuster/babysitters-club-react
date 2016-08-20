4e036c27edf559f872805afd53821c13bdf9c2c5
chriswigington <chriswigington@gmail.com>
2016-08-20 11:16:38 -0400

    Edited Search

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,16 +1,15 @@
 import $ from 'jquery'
 
 export default function search(props, currentUser) {
-   const URL = `http://localhost:3000/api/v1/parents/`
-  // console.log('we are creating a user!')
+  const URL = `http://localhost:3000/api/v1/parents/`
   const request = $.ajax({
-     url:URL,
-      headers: {'AUTHORIZATION': "Bearer" + currentUser.jwt},
-     type:"GET",
-     data: {parents: props},
-     contentType:"application/json; charset=utf-8",
-     dataType:"json",
-     context: this
+    url:URL,
+    headers: {'AUTHORIZATION': "Bearer" + currentUser.jwt},
+    type:"GET",
+    data: {parents: props},
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    context: this
    })
 
    return {
