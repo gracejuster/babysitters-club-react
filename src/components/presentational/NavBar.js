1cf7e3117cac5fb9b08f1aa11b38af9d17ba8235
grahk93 <grace.juster@flatironschool.com>
2016-08-23 19:43:04 -0400

    some progress on nav bar

--- src/components/presentational/NavBar.js
+++ src/components/presentational/NavBar.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux'
 import { Nav, NavItem } from 'react-bootstrap';
 let NavBar = ({type, username}) => {
   return(
-    <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+    // <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+    <Nav bsStyle="pills">
       <NavItem eventKey={1} href={`/${type.toLowerCase()}/${username}`}>Dashboard</NavItem>
       <NavItem eventKey={2} href={`/edit/${type.toLowerCase()}/${username}`}>Edit profile</NavItem>
       <NavItem eventKey={3} href="login">Log out</NavItem>
