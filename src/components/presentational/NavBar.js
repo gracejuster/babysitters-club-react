c0c47ad96452af397580be46ee2d4b90b1d247cf
grahk93 <grace.juster@flatironschool.com>
2016-08-23 19:39:49 -0400

    BOOTS@git add .git add .

--- src/components/presentational/NavBar.js
+++ src/components/presentational/NavBar.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Nav, NavItem } from 'react-bootstrap';
 let NavBar = ({type, username}) => {
   return(
-    <div className='NavBar'>
-      <form id="form" action={`/${type.toLowerCase()}/${username}`}>
-        <input id="submit" type="submit" className="btn btn-primary" value="DASHBOARD" />
-      </form>
-      <form id="form" action={`/edit/${type.toLowerCase()}/${username}`}>
-      {/* don't forget to change this according to which user is seen */}
-        <input id="submit" type="submit" className="btn btn-primary" value="EDIT PROFILE" />
-      </form>
-      <form id="form" action='/login'>
-        <input id="submit" type="submit" className="btn btn-primary" value="LOG OUT" />
-      </form>
-    </div>
+    <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+      <NavItem eventKey={1} href={`/${type.toLowerCase()}/${username}`}>Dashboard</NavItem>
+      <NavItem eventKey={2} href={`/edit/${type.toLowerCase()}/${username}`}>Edit profile</NavItem>
+      <NavItem eventKey={3} href="login">Log out</NavItem>
+    </Nav>
   )
 }
 function mapStateToProps(state) {
