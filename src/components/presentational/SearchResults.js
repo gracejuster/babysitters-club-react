38b2fd690132128a79dcdaf3b8aee9ed53e7ba99
supermancbs <clark.sacktor@gmail.com>
2016-08-24 19:23:47 -0400

    results now displays a message

--- src/components/presentational/SearchResults.js
+++ src/components/presentational/SearchResults.js
@@ -5,11 +5,20 @@ let SearchResults = ({searchResultsObject, currentUser}) => {
 	let SearchResultsList = searchResultsObject.map((searchResultItem) => {
 		return <ParentCard currentUser={currentUser} name={searchResultItem.attributes.name} kid_count={searchResultItem.attributes['kid-count']} email={searchResultItem.attributes.email} userId={searchResultItem.attributes['user-id']}/>
 	})
-	return (
-		<div className='SearchResults'>
-  		{SearchResultsList}
-		</div>
-	)
+	if (SearchResultsList.length===0){
+		return (
+			<div className='SearchResults'>
+				<center><h2>There are no parents that match that name.</h2></center>
+			</div>
+		)
+	}
+	else{
+		return (
+			<div className='SearchResults'>
+	  		{SearchResultsList}
+			</div>
+		)
+	}
 }
 
 SearchResults.propTypes = {
